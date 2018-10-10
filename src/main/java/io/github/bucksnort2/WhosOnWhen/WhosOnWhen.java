dc96340c0e6b5ec3402b6f9c612e608ef70915b2
bucksnort2 <smi14048@byui.edu>
2018-10-10 12:20:07 -0600

    IDK if there are any changes, this is up to date though

--- src/main/java/io/github/bucksnort2/WhosOnWhen/WhosOnWhen.java
+++ src/main/java/io/github/bucksnort2/WhosOnWhen/WhosOnWhen.java
@@ -11,36 +11,37 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class WhosOnWhen extends JavaPlugin{
+public final class WhosOnWhen extends JavaPlugin {
 	Map<Player, PlayerInformation> allPlayerInfo;
-	
+
 	@Override
 	public void onEnable() {
 		getLogger().info("\"Who's on When\" has been enabled!");
 		allPlayerInfo = new LinkedHashMap<Player, PlayerInformation>();
+
 	}
-	
+
 	@Override
 	public void onDisable() {
 		getLogger().info("\"Who's on When\" has been disabled!");
 		allPlayerInfo.clear();
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(sender instanceof Player) {
+		if (sender instanceof Player) {
 			Player p = (Player) sender;
 			PlayerInformation pInfo = allPlayerInfo.get(p);
 			pInfo.addCommand(cmd);
 			allPlayerInfo.put(p, pInfo);
 		}
-		
+
 		if (cmd.getName().equalsIgnoreCase("whoson")) {
 			if (sender instanceof Player) {
 				sender.sendMessage("This command can only be run by the console.");
 				return false;
 			}
-			for(Player p : allPlayerInfo.keySet()) {
+			for (Player p : allPlayerInfo.keySet()) {
 				PlayerInformation pInfo = allPlayerInfo.get(p);
 				System.out.println(p.getName());
 				pInfo.display();
@@ -49,7 +50,7 @@ public final class WhosOnWhen extends JavaPlugin{
 		}
 		return false;
 	}
-	
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player p = event.getPlayer();
@@ -58,7 +59,7 @@ public final class WhosOnWhen extends JavaPlugin{
 		pInfo.setStartTime(startTime);
 		allPlayerInfo.put(p, pInfo);
 	}
-	
+
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		Player p = event.getPlayer();
